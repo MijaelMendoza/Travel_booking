60662c39d8e7f3d3adeb63530867b1e2fb94cda8
Ignacio <abelignaciogarcia79@gmail.com>
2024-12-01 15:51:19 -0400

    arreglo login

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,24 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:carretera/auth_service/signin_page.dart';
+import 'package:carretera/components/home_page.dart'; // Asegúrate de importar la pantalla principal
 import 'package:device_preview/device_preview.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
+
   // Inicializa Firebase usando las opciones específicas para la plataforma
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
   // Configura la persistencia en plataformas web
- 
+  await FirebaseAuth.instance.setPersistence(Persistence.LOCAL);
+
   runApp(
     DevicePreview(
       enabled: !true, // Cambia a true para habilitar el Device Preview
@@ -36,10 +38,39 @@ class MyApp extends StatelessWidget {
         splash: Image.asset(
           'assets/images/nebulalogo.jpg',
         ),
-        nextScreen: SignInPage(),
+        nextScreen:
+            AuthWrapper(), // Cambia la siguiente pantalla al AuthWrapper
         splashTransition: SplashTransition.slideTransition,
         duration: 2000,
       ),
+    );
+  }
+}
+
+// Widget que decide si el usuario va a HomePage o SignInPage
+class AuthWrapper extends StatelessWidget {
+  const AuthWrapper({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        // Si el usuario ya está autenticado, mostrar HomePage
+        if (snapshot.connectionState == ConnectionState.active) {
+          final User? user = snapshot.data;
+          if (user != null) {
+            return const HomePage(); // Asegúrate de definir esta pantalla
+          } else {
+            return const SignInPage();
+          }
+        }
+
+        // Mostrar un indicador de carga mientras se verifica la autenticación
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      },
     );
   }
 }
